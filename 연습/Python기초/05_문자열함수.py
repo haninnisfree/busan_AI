7dc9f65d7cd71cc335338f788ed908e01656c67c
haninnisfree <dlsgml7343@gmail.com>
2025-03-07 15:03:08 +0900

    쉬는시간

---
+++ 연습/Python기초/05_문자열함수.py
@@ -0,0 +1,17 @@
+a = [1,2,3,['a','b',[1,3,5],'c']]
+print(a)
+print(type(a))
+print(a[0])
+print(a[3][1])
+print(a[3][2][1:])
+
+# split은 list로 출력
+jumin = '000212-4444444'
+print(jumin.split()) # list로 출력
+
+# 리스트 연산하기 (*, + 가능)
+
+
+# 
+
+
